cfe84bf057e1de93014ac4258a8abd7a0228c535
Vamshigutha <79559118+guthaVamshi@users.noreply.github.com>
2025-08-19 12:32:19 -0400

    updated Key related

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  DEEPGRAM_API_KEY: process.env.DEEPGRAM_API_KEY || "23d67a9abe0c12297afa9aea491d6d1381ec00d6",
+  DEEPGRAM_API_KEY: process.env.DEEPGRAM_API_KEY,
 };
 
 export default config;
